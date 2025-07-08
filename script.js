75bf94f1443ff5de9255d832cc6391b7f5a69b74
Sulemank-1 <suleman.dev42@gmail.com>
2025-07-09 00:59:18 +0500

    Add book contructor and addition to library function
    Write the book object contructor and a function
    which adds a book to the library array.

---
+++ script.js
@@ -0,0 +1,14 @@
+const myLibrary = [];
+
+function Book(title, author, id) {
+  this.title =  title;
+  this.author = author;
+  this.id = id;
+}
+
+function addBookToLibrary(title, author, id) {
+    const newBook = new Book(title, author, id);
+    myLibrary.push(newBook);
+}
+
+
