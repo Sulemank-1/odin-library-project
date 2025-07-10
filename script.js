6c358ba1f726842d0c86fbbfcb504e4b535ce02f
Sulemank-1 <suleman.dev42@gmail.com>
2025-07-10 21:02:19 +0500

    Write a function to display books
    Write a function that loops through the array and
    displays each book on the page.

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 const myLibrary = [];
+const containerDiv = document.querySelector(".container");
 
 function Book(title, author, id) {
-  this.title =  title;
+  this.title = title;
   this.author = author;
   this.id = id;
 }
 
 function addBookToLibrary(title, author, id) {
-    const newBook = new Book(title, author, id);
-    myLibrary.push(newBook);
+  const newBook = new Book(title, author, id);
+  myLibrary.push(newBook);
 }
 
 
+function displayBooks(library) {
+  for (let i = 0; i < library.length; i++) {
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("card");
+    const titleP = document.createElement("p");
+    titleP.classList.add("title");
+    const authorP = document.createElement("p");
+    authorP.classList.add("author");
+    
+    containerDiv.appendChild(cardDiv);
+    cardDiv.appendChild(titleP);
+    cardDiv.appendChild(authorP);
+    
+    titleP.innerText = myLibrary[i].title;
+    authorP.innerText = myLibrary[i].author;
+  }
+}
+
+displayBooks(myLibrary);
