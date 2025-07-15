33f4a316b11d57b6242b2cfcbe3eccd3a28d1ff1
Sulemank-1 <suleman.dev42@gmail.com>
2025-07-15 23:19:21 +0500

    Add a “New Book” button
    The button should bring up a form allowing users
    to input the details for the new book and add it
    to the library

--- script.js
+++ script.js
@@ -1,34 +1,66 @@
 const myLibrary = [];
 const containerDiv = document.querySelector(".container");
 
-function Book(title, author, id) {
+function Book(title, author, numberOfPages) {
   this.title = title;
   this.author = author;
-  this.id = id;
+  this.numberOfPages = numberOfPages;
 }
 
-function addBookToLibrary(title, author, id) {
-  const newBook = new Book(title, author, id);
+function addBookToLibrary(title, author, numberOfPages) {
+  const newBook = new Book(title, author, numberOfPages);
   myLibrary.push(newBook);
 }
 
-
 function displayBooks(library) {
-  for (let i = 0; i < library.length; i++) {
-    const cardDiv = document.createElement("div");
-    cardDiv.classList.add("card");
-    const titleP = document.createElement("p");
-    titleP.classList.add("title");
-    const authorP = document.createElement("p");
-    authorP.classList.add("author");
-    
-    containerDiv.appendChild(cardDiv);
-    cardDiv.appendChild(titleP);
-    cardDiv.appendChild(authorP);
-    
-    titleP.innerText = myLibrary[i].title;
-    authorP.innerText = myLibrary[i].author;
-  }
+  const i = library.length - 1;
+  const cardDiv = document.createElement("div");
+  cardDiv.classList.add("card");
+  const titleP = document.createElement("p");
+  titleP.classList.add("title");
+  const authorP = document.createElement("p");
+  authorP.classList.add("author");
+  const numberOfPagesP = document.createElement("p");
+  numberOfPagesP.classList.add("numberOfPages");
+
+  containerDiv.appendChild(cardDiv);
+  cardDiv.appendChild(titleP);
+  cardDiv.appendChild(authorP);
+  cardDiv.appendChild(numberOfPagesP);
+
+  titleP.innerText = myLibrary[i].title;
+  authorP.innerText = myLibrary[i].author;
+  numberOfPagesP.innerText = myLibrary[i].numberOfPages;
 }
 
-displayBooks(myLibrary);
+const dialog = document.querySelector(".dialog");
+const addBookButton = document.querySelector("#add-book-button");
+const closeButton = document.querySelector("#close-button");
+const titleInput = document.querySelector("[name='title']");
+const authorInput = document.querySelector("[name='author']");
+const numberOfPagesInput = document.querySelector('[name="number-of-pages"]');
+
+addBookButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
+
+const form = document.querySelector("#form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  addBookToLibrary(
+    titleInput.value,
+    authorInput.value,
+    numberOfPagesInput.value
+  );
+  displayBooks(myLibrary);
+
+  titleInput.value = "";
+  authorInput.value = "";
+  numberOfPagesInput.value = "";
+});
