e2a0d33d4727776587e0ef4dcbd43281aa37838e
Sulemank-1 <suleman.dev42@gmail.com>
2025-08-02 12:34:08 +0500

    Add read status button
    Add a button on each book’s display to change its read status

--- script.js
+++ script.js
@@ -8,37 +8,55 @@ function Book(title, author, numberOfPages, bookId) {
   this.bookId = bookId;
 }
 
+Book.prototype.readStatus = function () {
+  this.read = false;
+};
+
 function addBookToLibrary(title, author, numberOfPages, bookId) {
   const newBook = new Book(title, author, numberOfPages, bookId);
+  newBook.readStatus(true);
   myLibrary.push(newBook);
 }
 
 function displayBooks(library) {
   const i = library.length - 1;
+
   const cardDiv = document.createElement("div");
   cardDiv.setAttribute("class", "card");
+
   const titleP = document.createElement("p");
   titleP.setAttribute("class", "title");
+
   const authorP = document.createElement("p");
   authorP.setAttribute("class", "author");
+
   const numberOfPagesP = document.createElement("p");
   numberOfPagesP.setAttribute("class", "numberOfPages");
+
   const removeButton = document.createElement("button");
   removeButton.textContent = "remove";
   removeButton.setAttribute("class", "remove-button");
-  removeButton.setAttribute('data-book-id', myLibrary[i].bookId)
+
+  const readbtn = document.createElement("button");
+  readbtn.setAttribute("class", "read-btn");
+  readbtn.textContent = myLibrary[i].read ? "read" : "not read";
 
   containerDiv.appendChild(cardDiv);
   cardDiv.appendChild(titleP);
   cardDiv.appendChild(authorP);
   cardDiv.appendChild(numberOfPagesP);
+  cardDiv.appendChild(readbtn);
   cardDiv.appendChild(removeButton);
-  cardDiv.setAttribute("data-book-id", myLibrary[i].bookId);
 
-  removeButton.addEventListener('click', function(){
+  readbtn.addEventListener("click", function () {
+    myLibrary[i].read = !myLibrary[i].read;
+    readbtn.textContent = myLibrary[i].read ? "read" : "not read";
+  });
+
+  removeButton.addEventListener("click", function () {
     cardDiv.remove();
-    myLibrary.splice(i, 1);    
-  })
+    myLibrary.splice(i, 1);
+  });
 
   titleP.innerText = myLibrary[i].title;
   authorP.innerText = myLibrary[i].author;
@@ -64,21 +82,19 @@ const form = document.querySelector("#form");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  
+
   const randomBookId = crypto.randomUUID();
-  
+
   addBookToLibrary(
     titleInput.value,
     authorInput.value,
     numberOfPagesInput.value,
     randomBookId
   );
-  
+
   displayBooks(myLibrary);
 
   titleInput.value = "";
   authorInput.value = "";
   numberOfPagesInput.value = "";
-
- 
 });
